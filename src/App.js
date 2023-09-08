292fa09b66bcba6a098e9980f420a4700901157a
Vijay <vijaynaga.0503@gmail.com>
2023-09-08 06:59:57 +0530

    page navigation changes

--- src/App.js
+++ src/App.js
@@ -33,19 +33,13 @@ function App() {
       </div>
       <div className='flex mt-4'>
         {activePage !== 1 && (
-          <button
-            className='mx-2  absolute hover:opacity-70 h-screen opacity-[0.02] top-[0%] text-red-500 rotate-180 z-[100]'
-            onClick={() => handlePageChange(activePage - 1)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className='bg-white  rounded-full ' width="44" height="44" fill="none" viewBox="0 0 24 24" id="right-arrow"><path fill="#000" fill-rule="evenodd" d="M12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 16.6944 7.30558 20.5 12 20.5C16.6944 20.5 20.5 16.6944 20.5 12C20.5 7.30558 16.6944 3.5 12 3.5ZM2.5 12C2.5 6.75329 6.75329 2.5 12 2.5C17.2467 2.5 21.5 6.75329 21.5 12C21.5 17.2467 17.2467 21.5 12 21.5C6.75329 21.5 2.5 17.2467 2.5 12Z" clip-rule="evenodd"></path><path fill="#000" fill-rule="evenodd" d="M6.5 12C6.5 11.7239 6.72386 11.5 7 11.5H16.5C16.7761 11.5 17 11.7239 17 12C17 12.2761 16.7761 12.5 16.5 12.5H7C6.72386 12.5 6.5 12.2761 6.5 12Z" clip-rule="evenodd"></path><path fill="#000" fill-rule="evenodd" d="M13.1464 8.14645C13.3417 7.95118 13.6583 7.95118 13.8536 8.14645L17.3536 11.6464C17.5488 11.8417 17.5488 12.1583 17.3536 12.3536L13.8536 15.8536C13.6583 16.0488 13.3417 16.0488 13.1464 15.8536C12.9512 15.6583 12.9512 15.3417 13.1464 15.1464L16.2929 12L13.1464 8.85355C12.9512 8.65829 12.9512 8.34171 13.1464 8.14645Z" clip-rule="evenodd"></path></svg>
+          <button className='mx-2 cursor-default absolute hover:opacity-70 h-screen opacity-[0.02] top-[0%] text-red-500 rotate-180 z-[100]'>
+            <svg onClick={() => handlePageChange(activePage - 1)} xmlns="http://www.w3.org/2000/svg" className='bg-white rounded-full cursor-pointer' viewBox="0 0 40 40" width="44" height="44" id="arrow"><path d="M16.8 29c-.3 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l7.3-7.3-7.3-7.3c-.4-.4-.4-1 0-1.4s1-.4 1.4 0l8 8c.4.4.4 1 0 1.4l-8 8c-.2.2-.5.3-.7.3z"></path><path d="M20 40C9 40 0 31 0 20S9 0 20 0c4.5 0 8.7 1.5 12.3 4.2.4.3.5 1 .2 1.4-.3.4-1 .5-1.4.2C27.9 3.3 24 2 20 2 10.1 2 2 10.1 2 20s8.1 18 18 18 18-8.1 18-18c0-3.2-.9-6.4-2.5-9.2-.3-.5-.1-1.1.3-1.4.5-.3 1.1-.1 1.4.3C39 12.9 40 16.4 40 20c0 11-9 20-20 20z"></path></svg>
           </button>
         )}
         {activePage !== 5 && (
-          <button
-            className='mx-2 absolute hover:opacity-70 opacity-[0.02] h-screen top-[0%] right-0 text-red-500 z-[100]'
-            onClick={() => handlePageChange(activePage + 1)}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className='bg-white rounded-full' width="44" height="44" fill="none" viewBox="0 0 24 24" id="right-arrow"><path fill="#000" fill-rule="evenodd" d="M12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 16.6944 7.30558 20.5 12 20.5C16.6944 20.5 20.5 16.6944 20.5 12C20.5 7.30558 16.6944 3.5 12 3.5ZM2.5 12C2.5 6.75329 6.75329 2.5 12 2.5C17.2467 2.5 21.5 6.75329 21.5 12C21.5 17.2467 17.2467 21.5 12 21.5C6.75329 21.5 2.5 17.2467 2.5 12Z" clip-rule="evenodd"></path><path fill="#000" fill-rule="evenodd" d="M6.5 12C6.5 11.7239 6.72386 11.5 7 11.5H16.5C16.7761 11.5 17 11.7239 17 12C17 12.2761 16.7761 12.5 16.5 12.5H7C6.72386 12.5 6.5 12.2761 6.5 12Z" clip-rule="evenodd"></path><path fill="#000" fill-rule="evenodd" d="M13.1464 8.14645C13.3417 7.95118 13.6583 7.95118 13.8536 8.14645L17.3536 11.6464C17.5488 11.8417 17.5488 12.1583 17.3536 12.3536L13.8536 15.8536C13.6583 16.0488 13.3417 16.0488 13.1464 15.8536C12.9512 15.6583 12.9512 15.3417 13.1464 15.1464L16.2929 12L13.1464 8.85355C12.9512 8.65829 12.9512 8.34171 13.1464 8.14645Z" clip-rule="evenodd"></path></svg>
+          <button className='mx-2 cursor-default absolute hover:opacity-70 opacity-[0.02]  h-screen top-[0%] right-0 text-red-500 z-[100]'>
+            <svg onClick={() => handlePageChange(activePage + 1)} xmlns="http://www.w3.org/2000/svg" className='bg-white rounded-full cursor-pointer' viewBox="0 0 40 40" width="44" height="44" id="arrow"><path d="M16.8 29c-.3 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l7.3-7.3-7.3-7.3c-.4-.4-.4-1 0-1.4s1-.4 1.4 0l8 8c.4.4.4 1 0 1.4l-8 8c-.2.2-.5.3-.7.3z"></path><path d="M20 40C9 40 0 31 0 20S9 0 20 0c4.5 0 8.7 1.5 12.3 4.2.4.3.5 1 .2 1.4-.3.4-1 .5-1.4.2C27.9 3.3 24 2 20 2 10.1 2 2 10.1 2 20s8.1 18 18 18 18-8.1 18-18c0-3.2-.9-6.4-2.5-9.2-.3-.5-.1-1.1.3-1.4.5-.3 1.1-.1 1.4.3C39 12.9 40 16.4 40 20c0 11-9 20-20 20z"></path></svg>
           </button>
         )}
       </div>
