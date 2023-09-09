a44e25d0a08c85b9af6f74de598f3265bc14f393
Vijay <vijaynaga.0503@gmail.com>
2023-09-09 20:03:03 +0530

    page3 update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import './index.css';
 import Page1 from './Page1';
 import Page2 from './Page2';
-import Page3 from './Page4';
-import Page4 from './Page3';
+import Page3 from './Page3';
+import Page4 from './Page4';
 import Page5 from './Page5';
 
 function App() {
@@ -11,12 +11,11 @@ function App() {
 
   return (
     <div className='flex flex-col w-full h-screen overflow-auto'>
-      {/* <Page1 /> */}
-      <Page2 />
       <Page3 />
-      <Page4 />
-      <Page5 />
+      {/* <Page4 /> */}
+      {/* <Page5 /> */}
       <Page1 />
+      <Page2 />
     </div>
   );
 }
--- src/Page3.js
+++ src/Page3.js
@@ -32,7 +32,7 @@ export default function Page3() {
     return (
 
         <main>
-            <div className="App_Page3  relative flex w-full overflow-hidden bg-[#00d54b]  h-screen text-white">
+            <div className="App_Page3 max-[701px]:hidden  relative flex w-full overflow-hidden bg-[#00d54b]  h-screen text-white">
 
                 <div className='w-full flex grow justify-between  text-white '>
 
@@ -72,142 +72,36 @@ export default function Page3() {
             </div >
 
 
-            <div className="AppMobile hidden  flex flex-col w-full overflow-hidden items-center  bg-black h-screen text-white">
+            <div className="App_Page3 min-[701px]:hidden  relative flex w-full overflow-hidden bg-[#00d54b]  h-screen text-white">
 
-                <nav className='flex w-full px-[4%] py-[1%] items-center justify-between bg-gray-700'>
+                <div className='w-full flex grow justify-between  text-white '>
 
-                    <div className='w-[5%]'>
-                        <img className='w-[100%]' src={LOGO} alt="Logo" />
+                    <div className='flex flex-col  justify-between items-center abslute w-[50%]'>
+                        <div><img className='w-[100%]' src={BANKINGHOLE} alt="Phone" /></div>
+                        <div><img className='w-[80%]' src={BANKINGCOLUMN} alt="Phone" /></div>
+                        <div><img className='w-[75%] pl-4' src={BANKINGSTAIRS2} alt="Phone" />  </div>
                     </div>
 
-                    <div className="relative text-xl">
-                        <button
-                            className="text-[#00D54B]"
-                            onClick={toggleMenu}
-                        >
-                            ☰ Menu
-                        </button>
-                        {menuOpen && (
-                            <ul className="absolute text-base ml-3 mt-1 px-1 text-[#00D54B] bg-black border border-green-300 rounded shadow-lg">
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        SIGN IN
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500    w-full">
-                                        LEGAL
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        LICENSES
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        SECURITY
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        CAREERS
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        PRESS
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        SUPPORT
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        STATUS
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
-                                        CODEBLOG
-                                    </button>
-                                </li>
-                            </ul>
-                        )}
+
+                    <div className='flex flex-col justify-between items-center absoute w-[50%]  '>
+                        <div className=' floating-icon  w-[100%]  pt-32 text-left  transition-all duration-500 ease-in-out transition-transform'>
+                            <label className='text-white text-4xl mb-[10%]'>Banking</label>
+                            <p className='text-black text-lg mt-[2%] font-AgrandirNarrow '>Receive your paycheck, tax returns, and other direct deposits up to two days early using your Cash App routing
+                                and account number. </p>
+                        </div>
+
+                        <div><img className='w-[100%] ' src={BANKINGPHONE} alt="Phone" />  </div>
+                        <div><img className='w-[60%] ' src={BANKINGTUBE} alt="Phone" /></div>
                     </div>
 
-                    <div className='w-[10%]'>
-                        <img className='w-[100%] ml-[0%]' src={EYE} alt="Eye" />
+
+                    <div className='flex flex-col relative justify-between items-center pr-4 w-[40%]  abslute'>
+                        <div><img className='w-[100%] absolute right-0 mr-4' src={BANKINGTRACK2} alt="Phone" /></div>
+                        <div><img className='w-[100%] ' src={BANKINGSTAIRS1} alt="Phone" /></div>
+                        <div><img className='w-[100%] pb-12' src={BANKINGMONSTER} alt="Phone" />  </div>
                     </div>
-                </nav>
 
-                {/* <div className='w-full grow flex flex-col mt-[50%] justify-betwen '>
-
-    <div className='flex w-full  justify-between items-start text-white  floating-icon'>
-        <img className=' ml-[12%] w-[6%]' src={CUBE} alt="Phone" />
-        <img className='w-[15%] mr-[15%]' src={STAIRS} alt="Phone" />
-    </div>
-
-    <div className='-mt-[10%] -mb-[4%] -ml-[6%]  relative flex justify-center items-center text-white '>
-        <div className='w-[48%] flex flex-col items-center'>
-            <img className='w-[84%]' src={CASH} alt="Cash" />
-            <img className=' invisible w-[10%]' src={CASH} alt="Cash" />
-
-            <div className='w-[49%] absolute -top-[22%] left-[38.7%] '>
-                <img className='w-[48%]' src={PHONE} alt="Phone" />
-            </div>
-
-            <img className='relative w-[63%]' src={APP} alt="App" />
-        </div>
-    </div>
-
-    <div className='w-full -mb-[0%]  flex justify-between  items-start text-white  floating-icon'>
-        <img className='w-[14%] ml-[12%]' src={CUBES} alt="Phone" />
-        <img className='w-[22%]  mr-[22%]' src={INTROPILLAR} alt="Phone" />
-    </div>
-</div> */}
-
-                {/* <nav className='w-full text-white absolute bottom-0  flex justify-between items-center  px-14'>
-
-    <div className='flex '>
-        <ul className='flex gap-5 font-extrabold text-xs '>
-            <button className='flex hover:-translate-y-1 hover:scale-105 transition-all duration-700ms gap-2 items-end px-6 py-3.5 rounded-lg border-2 border-white bg-black'>
-                <span>
-                    <img className='w-[100%]' src={APPLE} alt="Phone" />
-                </span>APP STORE</button>
-            <button className='flex gap-2 hover:-translate-y-1 hover:scale-105 transition-all duration-700ms items-end px-4 py-3.5 rounded-lg border-2 border-white  bg-black'>
-                <span>
-                    <img className='w-[100%]' src={STORE} alt="Phone" />
-                </span>GOOGLE PLAY</button>
-        </ul>
-    </div>
-
-    <div className='flex animate-bounce pr-2 pl-8'>
-<img tabIndex={0} className='w-[100%]' src={ARROW} alt="Phone" />
-</div>
-
-    <div className='flex '>
-        <div className='flex pr-4 pb-2 w-[410px]'>
-            <p className=' font-AgrandirLight font-semibold text-xs  text-white text-left m-0'>Brokerage services by Cash App Investing LLC, member FINRA / SIPC.
-                See our BrokerCheck. Investing involves risk; you may lose money. Bitcoin trading offered by Cash App. Cash App Investing does not trade bitcoin and Cash App is not a member of FINRA or SIPC. Cash App facilitates banking services through Sutton Bank and Lincoln Savings Bank, Members FDIC.</p>
-        </div>
-
-        <ul className='flex gap-5 font-extrabold	text-xs'>
-            <button>
-                <img className='w-[100%] ' src={THREAD} alt="Phone" />
-            </button>
-            <button>
-                <img className='w-[100%] ' src={TWEET} alt="Phone" />
-            </button>
-            <button>
-                <img className='w-[100%] ' src={INSTA} alt="Phone" />
-            </button>
-        </ul>
-    </div>
-</nav> */}
-
+                </div>
             </div >
         </main >
     )
