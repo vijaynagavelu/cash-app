662f809dac0b7bd5229a9f747d414757f1be09fa
Vijay <vijaynaga.0503@gmail.com>
2023-09-10 01:27:19 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
 
   return (
     <div className='flex flex-col w-full h-screen overflow-auto'>
-      <Page5 />
       <Page1 />
       <Page2 />
       <Page3 />
       <Page4 />
+      <Page5 />
     </div>
   );
 }
