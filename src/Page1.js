ee05e441e9701dedd7e2c73376243a20cfc22aeb
Vijay <vijaynaga.0503@gmail.com>
2023-09-09 11:10:52 +0530

    page 1 mobile update

--- src/Page1.js
+++ src/Page1.js
@@ -19,7 +19,7 @@ import THREAD from './assets_Page1/THREADS_ICON.png'
 
 
 import { useState } from 'react';
-// import ARROW from './assets_Page1/ARROW.png'
+import ARROW from './assets_Page1/ARROW.png'
 
 
 export default function Page1() {
@@ -125,15 +125,14 @@ export default function Page1() {
 
             </div >
 
-            <div className="AppMobile   flex flex-col w-full overflow-hidden items-center  bg-black h-screen text-white">
+            <div className="AppMobile flex flex-col w-full overflow-auto oveflow-x-hidden items-center  bg-black h-screen text-white">
 
-                <nav className='flex w-full px-[4%] py-[1%] items-center justify-between bg-gray-700'>
-
-                    <div className='w-[5%]'>
+                <nav className='flex w-full px-[4%] py-[4%]  items-center justify-between   border-b-[0.5px] border-gray-700 px-4'>
+                    <div className='w-[5%] '>
                         <img className='w-[100%]' src={LOGO} alt="Logo" />
                     </div>
 
-                    <div className="relative text-xl">
+                    <div className=" text-xl ">
                         <button
                             className="text-[#00D54B]"
                             onClick={toggleMenu}
@@ -141,49 +140,49 @@ export default function Page1() {
                             ☰ Menu
                         </button>
                         {menuOpen && (
-                            <ul className="absolute text-base ml-3 mt-1 px-1 text-[#00D54B] bg-black border border-green-300 rounded shadow-lg">
+                            <ul className="absolute z-[1] text-sm ml-0 mt-4 p-2 text-[#00D54B] bg-black border border-green-300 rounded shadow-lg">
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1 w-full">
                                         SIGN IN
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500    w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1   w-full">
                                         LEGAL
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1  w-full">
                                         LICENSES
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1  w-full">
                                         SECURITY
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1  w-full">
                                         CAREERS
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1  w-full">
                                         PRESS
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1  w-full">
                                         SUPPORT
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1  w-full">
                                         STATUS
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="block border-b-[0.5px] border-gray-500   w-full">
+                                    <button className="block border-b-[0.5px] border-gray-500  py-1  w-full">
                                         CODEBLOG
                                     </button>
                                 </li>
@@ -196,19 +195,18 @@ export default function Page1() {
                     </div>
                 </nav>
 
-                {/* <div className='w-full grow flex flex-col mt-[50%] justify-betwen '>
-
-                    <div className='flex w-full  justify-between items-start text-white  floating-icon'>
-                        <img className=' ml-[12%] w-[6%]' src={CUBE} alt="Phone" />
-                        <img className='w-[15%] mr-[15%]' src={STAIRS} alt="Phone" />
+                <div className='w-full  mt-[10%] -mb-[30%] grow flex flex-col  justify-evenly '>
+                    <div className='flex  w-full  justify-between items-start text-white  floating-icon'>
+                        <img className='w-[18%] ml-[12%]' src={CUBE} alt="Phone" />
+                        <img className='w-[20%] mr-[15%]' src={STAIRS} alt="Phone" />
                     </div>
 
-                    <div className='-mt-[10%] -mb-[4%] -ml-[6%]  relative flex justify-center items-center text-white '>
-                        <div className='w-[48%] flex flex-col items-center'>
-                            <img className='w-[84%]' src={CASH} alt="Cash" />
+                    <div className=' w-[100%]  relative flex justify-center items-center text-white '>
+                        <div className='flex flex-col items-center'>
+                            <img className='w-[84%] ' src={CASH} alt="Cash" />
                             <img className=' invisible w-[10%]' src={CASH} alt="Cash" />
 
-                            <div className='w-[49%] absolute -top-[22%] left-[38.7%] '>
+                            <div className='w-[100%]  absolute -top-[22%] left-[28%] '>
                                 <img className='w-[48%]' src={PHONE} alt="Phone" />
                             </div>
 
@@ -217,37 +215,64 @@ export default function Page1() {
                     </div>
 
                     <div className='w-full -mb-[0%]  flex justify-between  items-start text-white  floating-icon'>
-                        <img className='w-[14%] ml-[12%]' src={CUBES} alt="Phone" />
-                        <img className='w-[22%]  mr-[22%]' src={INTROPILLAR} alt="Phone" />
+                        <img className='w-[24%] ml-[5%]' src={CUBES} alt="Phone" />
+                        <img className='w-[28%]  mr-[5%]' src={INTROPILLAR} alt="Phone" />
                     </div>
-                </div> */}
+                </div>
 
-                {/* <nav className='w-full text-white absolute bottom-0  flex justify-between items-center  px-14'>
+                <div className='w-full hidden  mb-[10%] grow flex flex-col  justify-evenly '>
+                    <div className='flex  w-full  justify-between items-start text-white  floating-icon'>
+                        <img className='w-[18%] ml-[12%]' src={CUBE} alt="Phone" />
+                        <img className='w-[20%] mr-[15%]' src={STAIRS} alt="Phone" />
+                    </div>
 
-                    <div className='flex '>
-                        <ul className='flex gap-5 font-extrabold text-xs '>
-                            <button className='flex hover:-translate-y-1 hover:scale-105 transition-all duration-700ms gap-2 items-end px-6 py-3.5 rounded-lg border-2 border-white bg-black'>
+                    <div className=' w-[100%]  relative flex justify-center items-center text-white '>
+                        <div className='flex flex-col items-center'>
+                            <img className='w-[84%] ' src={CASH} alt="Cash" />
+                            <img className=' invisible w-[10%]' src={CASH} alt="Cash" />
+
+                            <div className='w-[100%]  absolute -top-[22%] left-[28%] '>
+                                <img className='w-[48%]' src={PHONE} alt="Phone" />
+                            </div>
+
+                            <img className='relative w-[63%]' src={APP} alt="App" />
+                        </div>
+                    </div>
+
+                    <div className='w-full -mb-[0%]  flex justify-between  items-start text-white  floating-icon'>
+                        <img className='w-[24%] ml-[5%]' src={CUBES} alt="Phone" />
+                        <img className='w-[28%]  mr-[5%]' src={INTROPILLAR} alt="Phone" />
+                    </div>
+                </div>
+
+                <nav className='w-full invisibl translate-y-44 absolut bg-black text-white  bottom-0  flex flex-col justify-between items-center'>
+                    <div className='flex flex-col text-left p-2'>
+                        <label className='pb-4'>Get it on</label>
+                        <ul className='flex justify-between gap-5 font-extrabold text-xs '>
+                            <button className='flex hover:-translate-y-1 hover:scale-105 transition-all duration-700ms gap-2 items-end px-2 py-1.5 rounded-lg border-2 border-white bg-black'>
                                 <span>
-                                    <img className='w-[100%]' src={APPLE} alt="Phone" />
+                                    <img className='w-[90%]' src={APPLE} alt="Phone" />
                                 </span>APP STORE</button>
-                            <button className='flex gap-2 hover:-translate-y-1 hover:scale-105 transition-all duration-700ms items-end px-4 py-3.5 rounded-lg border-2 border-white  bg-black'>
+                            <button className='flex gap-2 hover:-translate-y-1 hover:scale-105 transition-all duration-700ms items-end px-2 py-1.5 rounded-lg border-2 border-white  bg-black'>
                                 <span>
-                                    <img className='w-[100%]' src={STORE} alt="Phone" />
+                                    <img className='w-[90%]' src={STORE} alt="Phone" />
                                 </span>GOOGLE PLAY</button>
                         </ul>
                     </div>
 
-                    <div className='flex animate-bounce pr-2 pl-8'>
-        <img tabIndex={0} className='w-[100%]' src={ARROW} alt="Phone" />
-    </div>
 
-                    <div className='flex '>
-                        <div className='flex pr-4 pb-2 w-[410px]'>
-                            <p className=' font-AgrandirLight font-semibold text-xs  text-white text-left m-0'>Brokerage services by Cash App Investing LLC, member FINRA / SIPC.
+
+                    {/* <div className='flex animate-bounce pr-2 pl-8'>
+                        <img tabIndex={0} className='w-[100%]' src={ARROW} alt="Phone" />
+                    </div> */}
+
+                    <div className='flex flex-col'>
+                        <div className='flex p-2 '>
+                            <p className='border-b-[0.5px] border-gray-700 font-AgrandirLight pb-2 text-[12px]  text-white text-left '>Brokerage services by Cash App Investing LLC, member FINRA / SIPC.
                                 See our BrokerCheck. Investing involves risk; you may lose money. Bitcoin trading offered by Cash App. Cash App Investing does not trade bitcoin and Cash App is not a member of FINRA or SIPC. Cash App facilitates banking services through Sutton Bank and Lincoln Savings Bank, Members FDIC.</p>
                         </div>
 
-                        <ul className='flex gap-5 font-extrabold	text-xs'>
+                        <ul className='flex gap-5 font-extrabold p-2  pb-4 text-xs'>
                             <button>
                                 <img className='w-[100%] ' src={THREAD} alt="Phone" />
                             </button>
@@ -259,7 +284,7 @@ export default function Page1() {
                             </button>
                         </ul>
                     </div>
-                </nav> */}
+                </nav>
 
             </div >
 
